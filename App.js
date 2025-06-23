d0414fd050d64e50815e20e1ad80a1ce8dade4dc
Anshi269 <guptanshi4u@gmail.com>
2025-06-23 14:04:40 +0530

    working

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import React from "react";
+import 'react-native-url-polyfill/auto';
 import { NavigationContainer } from "@react-navigation/native";
+import { AppRegistry } from 'react-native';
 import OnboardingStack from "./navigation/Screens";
 
 console.log("🟢 App.js is running");
 
-export default function App() {
+function App() {
   return (
     <NavigationContainer>
       <OnboardingStack />
     </NavigationContainer>
   );
 }
+
+// Register the main component
+AppRegistry.registerComponent('main', () => App);
+
+export default App;
